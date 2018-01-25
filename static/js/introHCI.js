bdbbe82b287d113790144df0a5cc3ad0147413ea
Andy Xia <anxia@ucsd.edu>
2018-01-25 10:32:43 -0800

    finished lab 3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,27 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+	$('.jumbotron h1').text("let me out, let me out");
 	});
+	$("#testjs").toggleClass("active");
+	$(".jumbotron p").addClass("active");
 
+	$("#testjs").text("Please wait...");
+	$("a.thumbnail").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+function projectClick(e){
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+	 var containingProject = $(this).closest(".project");
+    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+}
+}
+
 }
